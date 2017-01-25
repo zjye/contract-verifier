9e77e3209116151a75be5c6e703275db0d300f53
jye <jye918@gmail.com>
2017-01-25 20:46:27 +0800

    listener test

--- consumer/src/main/java/com/zjye/consumer/Application.java
+++ consumer/src/main/java/com/zjye/consumer/Application.java
@@ -2,8 +2,11 @@ package com.zjye.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Sink;
 
 @SpringBootApplication
+@EnableBinding(Sink.class)
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
